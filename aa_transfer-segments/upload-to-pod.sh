d4d74b7d67225c9fcca325d28c3310d2254132de
Aravindo Wingeier <wingeier@adobe.com>
2020-02-28 12:10:33 +0100

    fixed mkdir

--- aa_transfer-segments/upload-to-pod.sh
+++ aa_transfer-segments/upload-to-pod.sh
@@ -1,2 +1,4 @@
 POD=$(kubectl get pods -l app=test-diskcache-aem-publish | grep Running | cut -d " " -f 1)
+kubectl exec -it  ${POD} mkdir -- -p /mnt/sandbox/cache/
+
 kubectl cp aem ${POD}:/mnt/sandbox/cache/aem

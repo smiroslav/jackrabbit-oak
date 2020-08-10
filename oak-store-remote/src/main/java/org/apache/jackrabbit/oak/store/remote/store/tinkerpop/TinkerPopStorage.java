180763f6b93f915293047418c4376631ce9bec0b
Miroslav Smiljanic <smiljani@adobe.com>
2020-08-10 17:04:57 +0200

    gremlin queries start

--- oak-store-remote/src/main/java/org/apache/jackrabbit/oak/store/remote/store/tinkerpop/TinkerPopStorage.java
+++ oak-store-remote/src/main/java/org/apache/jackrabbit/oak/store/remote/store/tinkerpop/TinkerPopStorage.java
@@ -1,25 +1,77 @@
 package org.apache.jackrabbit.oak.store.remote.store.tinkerpop;
 
-import org.apache.tinkerpop.gremlin.driver.Cluster;
-import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
+import org.apache.jackrabbit.oak.api.PropertyState;
+import org.apache.jackrabbit.oak.store.remote.store.Node;
+import org.apache.jackrabbit.oak.store.remote.store.Storage;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 
-import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeMap;
 
-public class TinkerPopStorage implements AutoCloseable {
-
-    private final Cluster cluster;
+public class TinkerPopStorage implements Storage {
 
     private final GraphTraversalSource g;
 
-    public TinkerPopStorage(int port) {
-        this.cluster = Cluster.build().port(port).create();
-        this.g = traversal().withRemote(DriverRemoteConnection.using(cluster));
+    public TinkerPopStorage(GraphTraversalSource g) {
+        this.g = g;
     }
 
+    @Override
+    public void addNode(String path, Iterable<? extends PropertyState> properties) {
+    }
 
     @Override
-    public void close() throws Exception {
-        cluster.close();
+    public void addNode(String path, Node node) {
+
+    }
+
+    @Override
+    public void deleteNode(String path, long revision) {
+
+    }
+
+    @Override
+    public void deleteNode(String path) {
+
+    }
+
+    @Override
+    public Node getNode(String path, long revision) {
+        return null;
+    }
+
+    @Override
+    public Node getRootNode() {
+        List<Vertex> list = g.V().hasLabel("root").toList();
+
+        if (list.isEmpty()) {
+            return null;
+        }
+
+        long revision = (long) g.V().hasLabel("root").elementMap().next().get("revision");
+        Node rootNode = new Node("/", Collections.emptyMap(), revision);
+        return rootNode;
+    }
+
+    @Override
+    public TreeMap<String, Node> getNodeAndSubtree(String path, long revision, boolean wholeSubtree) {
+        return null;
+    }
+
+    @Override
+    public void moveChildNodes(String fromPath, String toPath) {
+
+    }
+
+    @Override
+    public long incrementRevisionNumber() {
+        return 0;
+    }
+
+    @Override
+    public long getCurrentRevision() {
+        return 0;
     }
 }
--- oak-store-remote/src/test/java/org/apache/jackrabbit/oak/store/remote/store/tinkerpop/TinkerPopStorageTest.java
+++ oak-store-remote/src/test/java/org/apache/jackrabbit/oak/store/remote/store/tinkerpop/TinkerPopStorageTest.java
@@ -1,8 +1,13 @@
 package org.apache.jackrabbit.oak.store.remote.store.tinkerpop;
 
+import org.apache.jackrabbit.oak.store.remote.store.Node;
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.server.GremlinServer;
 import org.apache.tinkerpop.gremlin.server.Settings;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,18 +15,32 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 public class TinkerPopStorageTest {
 
     private static final String CONFIG = "org/apache/jackrabbit/oak/store/remote/store/tinkerpop/gremlin-server.yaml";
 
+    private TinkerPopStorage storage;
+
     private GremlinServer server;
+
+    private Cluster cluster;
+    private GraphTraversalSource g;
 
     @Before
     public void setUp() throws Exception {
         startServer();
+
+        cluster = Cluster.build().port(45940).create();
+
+        g = traversal().withRemote(DriverRemoteConnection.using(cluster));
+
+        storage = new TinkerPopStorage(g);
     }
 
     /**
@@ -36,20 +55,22 @@ public class TinkerPopStorageTest {
 
     @After
     public void tearDown() throws Exception {
-        stopServer();
-    }
-
-    /**
-     * Stops a current instance of Gremlin Server.
-     */
-    public void stopServer() throws Exception {
+        cluster.close();
         server.stop().join();
     }
 
 
-
     @Test
-    public void shouldCreateGraph() throws Exception {
+    public void testGerRootNode() throws Exception {
+        Node node = storage.getRootNode();
 
+        assertNull(node);
+
+        g.addV("root").property("revision", 1l).next();
+        g.addV("root").property("revision", 2l).next();
+
+        node = storage.getRootNode();
+
+        assertNotNull(node);
     }
 }

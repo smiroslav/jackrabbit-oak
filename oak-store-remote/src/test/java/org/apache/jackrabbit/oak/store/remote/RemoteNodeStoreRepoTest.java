ab4780eef558a1fa74855437aaed23436d400869
Miroslav Smiljanic <smiljani@adobe.com>
2020-10-01 11:00:57 +0200

    pipes

--- oak-store-remote/src/test/java/org/apache/jackrabbit/oak/store/remote/RemoteNodeStoreRepoTest.java
+++ oak-store-remote/src/test/java/org/apache/jackrabbit/oak/store/remote/RemoteNodeStoreRepoTest.java
@@ -1,5 +1,6 @@
 package org.apache.jackrabbit.oak.store.remote;
 
+import org.apache.commons.lang.CharEncoding;
 import org.apache.jackrabbit.commons.cnd.CndImporter;
 import org.apache.jackrabbit.commons.cnd.ParseException;
 import org.apache.jackrabbit.oak.jcr.Jcr;
@@ -25,11 +26,17 @@ import javax.jcr.SimpleCredentials;
 import javax.jcr.Value;
 import javax.jcr.ValueFactory;
 import javax.jcr.nodetype.NodeTypeManager;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.io.Reader;
 
 import static org.mockito.ArgumentMatchers.anyString;
@@ -59,6 +66,44 @@ public class RemoteNodeStoreRepoTest extends AbstractRemoteNodeStoreRepoTest{
         if(session != null) {
             session.logout();
         }
+    }
+
+    static final String SITEMAP_NAMESPACE = "http://www.sitemaps.org/schemas/sitemap/0.9";
+
+    @Test
+    public void test3() throws RepositoryException, IOException, ParseException {
+
+        Node node = session.getRootNode().addNode("a");
+
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+
+        new Thread(
+                new Runnable(){
+                    public void run(){
+                        try {
+                            XMLStreamWriter xmlWriter = XMLOutputFactory.newFactory().createXMLStreamWriter(out, CharEncoding.UTF_8);
+
+                            xmlWriter.writeStartDocument(CharEncoding.UTF_8, "1.0");
+                            xmlWriter.writeStartElement("", "sitemapindex", SITEMAP_NAMESPACE);
+
+                            xmlWriter.writeEndElement();
+
+                            xmlWriter.writeEndDocument();
+                            out.close();
+
+                        } catch (XMLStreamException | IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+        ).start();
+
+        node.setProperty("binaryProp",session.getValueFactory().createBinary(in));
+        in.close();
+        session.save();
+
     }
 
 }
